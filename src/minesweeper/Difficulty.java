0c8b4dc4c4b1f4b084a3179713c64a7b8162a529
georgescutelnicu <george.v.scutelnicu@gmail.com>
2024-03-08 00:51:14 +0200

    Refactor switch-case

--- src/minesweeper/Difficulty.java
+++ src/minesweeper/Difficulty.java
@@ -6,15 +6,11 @@ public class Difficulty {
     public static final String HARD = "Hard";
 
     public static int[] getSizeAndMines(String difficulty) {
-        switch (difficulty) {
-            case EASY:
-                return new int[]{10, 10};
-            case MEDIUM:
-                return new int[]{16, 40};
-            case HARD:
-                return new int[]{20, 70};
-            default:
-                return new int[]{10, 10};
-        }
+        return switch (difficulty) {
+            case EASY -> new int[]{10, 10};
+            case MEDIUM -> new int[]{16, 40};
+            case HARD -> new int[]{20, 70};
+            default -> throw new IllegalArgumentException("Unknown difficulty: " + difficulty);
+        };
     }
 }
